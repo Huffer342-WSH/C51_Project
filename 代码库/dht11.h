671cc79e44d951d44261127723421794d93f5932
Huffer342-WSH <718007138@qq.com>
2022-05-25 19:58:57 +0800

    添加宏作为引脚接口

--- 代码库/dht11.h
+++ 代码库/dht11.h
@@ -2,8 +2,8 @@
 #define __DHT11_H_
 
 #include "main.h"
-
-sbit DHT11_DIO = P1 ^ 0; // dht11data端接单片机的P1^0口//
+/*引脚*/
+#define DHT11_DIO DHT11_DIO
 
 uint8 DHT11_Read(uint8 *humidity, uint8 *temperature);
 #endif
