79bef6d8930121f3348c770a42531c88cb063203
Huffer342-WSH <718007138@qq.com>
2022-05-27 12:13:51 +0800

    修改目录

--- 练习1 - 副本/Core/delay.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DELAY_H_
-#define __DELAY_H_
-
-#define delayms Delayms
-#define delayus Delayus
-void Delayms(unsigned int t); //@11.0592MHz
-void Delayus(unsigned int t);
-
-#endif
--- 练习1 - 副本/Core/usart.h
+++
@@ -1,8 +0,0 @@
-#ifndef __USART_H_
-#define __USART_H_
-
-void Uart_Init(void);
-void SendData(unsigned char dat);
-void SendString(char *s);
-
-#endif
